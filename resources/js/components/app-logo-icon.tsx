61c8af90d6b861d04648d89e2107a972c2e03513
Karim Fayek Malak <30074501+karimfayek@users.noreply.github.com>
2025-12-07 16:43:49 +0200

    logo

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,6 +2,6 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<HTMLImageElement>) {
     return (
-        <img {...props} src="/logo.png" alt="Rafco Logo" style={{ width: '200px' }} />
+        <img {...props} src="/logonew.png" alt="Rafco Logo" style={{ width: '200px' }} />
     );
 }
